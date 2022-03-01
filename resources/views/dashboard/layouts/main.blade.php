144e3afbd5030a5add01088a92a8ae801a512413
Fardan Nozami <fardan.nozami@gmail.com>
2022-03-02 06:36:10 +0700

    create post bagian pertama done

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -14,6 +14,17 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+    {{-- trix editor --}}
+    <link rel="stylesheet" type="text/css" href="/css/trix.css">
+    <script type="text/javascript" src="/js/trix.js"></script>
+
+    <style>
+      trix-toolbar [data-trix-button-group='file-tools'] {
+        display: none;
+      }
+    </style>
+
   </head>
   <body>
     
@@ -32,6 +43,6 @@
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
       
-      <script src="/js/dashboard.js"></script>
+      {{-- <script src="/js/dashboard.js"></script> --}}
   </body>
 </html>
