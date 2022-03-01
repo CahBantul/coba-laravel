b851ff3ea668ca5cf087ac213c3e43cbdbc2ef42
Fardan Nozami <fardan.nozami@gmail.com>
2022-03-01 23:24:37 +0700

    halaman dashboard done

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +21,11 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
     return view('home', [
         "title" => "Home",
-        "active" => "home"
     ]);
 });
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
-        "active" => "about"
     ]);
 });
 
@@ -40,7 +38,6 @@ Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all(),
-        "active" => "categories"
     ]);
 });
 
@@ -51,4 +48,8 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
