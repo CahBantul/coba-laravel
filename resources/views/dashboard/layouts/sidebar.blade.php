a3280f61012dd4c734e98241b92ab1df5cd1c150
Fardan Nozami <fardan.nozami@gmail.com>
2022-03-02 07:33:21 +0700

    belum selesai post

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/posts*')? 'active' : '' }}" href="dashboard/posts">
+        <a class="nav-link {{ Request::is('dashboard/posts*')? 'active' : '' }}" href="/dashboard/posts">
           <span data-feather="file-text"></span>
           My Posts
         </a>
@@ -16,3 +16,4 @@
     </ul>
   </div>
 </nav>
+
