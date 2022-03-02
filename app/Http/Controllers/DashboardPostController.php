18e09866279a0a51868f108d79c34a05f53c6eb4
Fardan Nozami <fardan.nozami@gmail.com>
2022-03-02 22:29:28 +0700

    delete image done

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -131,6 +131,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
